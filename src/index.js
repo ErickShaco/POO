3d469cc7123b4bd00225c0ca14c8c132ef0148eb
Erick Medeiros <erick144m@gmail.com>
2025-01-07 23:33:17 +0000

    POO

---
+++ src/index.js
@@ -0,0 +1,62 @@
+/*Questão 1
+
+const pessoa = {
+    nome:"Jorge",
+    idade: 20,
+}
+function apresentar(){
+    return console.log(`Ola meu Nome é ${pessoa.nome} e Tenho ${pessoa.idade} de Idade`)
+}
+apresentar()*/
+
+/*Questão 2
+
+const livro = {
+    titulo: "Vagabond",
+    autor: "Takehiro Inoue",
+    anoPublicacao: 2014
+}
+
+function resumo(){
+    return console.log(`O Livro ${livro.titulo} foi escrito por ${livro.autor} em ${livro.anoPublicacao}`)
+}
+
+resumo()*/
+
+/*Questão 3
+
+const celular = {
+    marca: "Samsung",
+    modelo: "Galaxy Flip",
+    bateria: "15%"
+}
+
+function verificarBateria(){
+    return console.log(`A Bateria está em ${celular.bateria}`)
+}
+
+verificarBateria()*/
+
+/*Questão 4
+
+const carro ={
+    marca:"Honda",
+    modelo:"civc",
+    velocidade: 0
+}
+
+function acelerar(adc = 10){
+    carro.velocidade += adc
+
+    console.log(`Velocidade Atual:${carro.velocidade}KM`)
+
+
+}
+
+acelerar()*/
+
+const cachorro = {
+    nome: "Bankai",
+    raca: "Salsicha"
+}
+
